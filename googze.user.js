d09d3d1800bba66624a6d97ab71627811f5d69c9
rytis.s@gmail.com <rytis.s@gmail.com@b9383bd5-25d9-8f43-6282-5640fff02bcd>
2010-09-06 20:13:15 +0000

    Work in progress for handling more errors and implementing a US version.
    The problem with US version is that it uses a different settings for OpenLayers image tiles (even when converted to Google format), based on http://docs.openlayers.org/library/spherical_mercator.html
    
    setting, Waze, Google
    numZoomLevels, 10, 18
    maxExtent, (-90,-180,180,90), (-20037508.34,-20037508.34,20037508.34,20037508.34)
    
    Need to find a way to convert between these and possibly move Waze tiles to match Google...

--- googze.user.js
+++ googze.user.js
@@ -1,4 +1,4 @@
-﻿// ==UserScript==
+// ==UserScript==
 // @name         Googze
 // @namespace    http://code.google.com/p/googze
 // @description  Show aerial photos from Google Maps in Waze cartouche editor
@@ -9,18 +9,57 @@
 var wazeToGoogle = function() {
 	try {
 		setTimeout(wazeToGoogle, 4000);
-		var rE = /\/get_datadoors_tile\/(?:\d+)\/L(\d+)\/R(\w+)\/C(\w+)\.JPG/i; // Reg Expression
-		var nodes = document.getElementById("OpenLayers.Layer.ArcGISCache_8").childNodes;
-		for (var i in nodes) {
-			var src = nodes[i].firstChild.src;
-			var matches = src.match(rE);
-			if (matches != null) {
-				matches[2] = parseInt(matches[2], 16);
-				matches[3] = parseInt(matches[3], 16);
-				var rP = 'http://khm0.google.com/kh/v=67&x=' + matches[3] + '&y=' + matches[2] + '&z=' + matches[1] + '&s=Galileo'; // Replacement of the Reg Expression
-				nodes[i].firstChild.setAttribute("src", rP);
+		var rWorld = /\/get_datadoors_tile\/(?:\d+)\/L(\d+)\/R(\w+)\/C(\w+)\.JPG/i; // Reg Expression for World server
+
+		var nWorld = document.getElementById("OpenLayers.Layer.ArcGISCache_8");
+		if (nWorld != null) {
+			nWorld = nWorld.childNodes; // DOM nodes for World server
+		
+			// Replace World server
+			for (var i in nWorld) {
+				var src = nWorld[i].firstChild.src;
+				var matches = src.match(rWorld);
+				if (matches != null) {
+					matches[2] = parseInt(matches[2], 16);
+					matches[3] = parseInt(matches[3], 16);
+					var rP = 'http://khm0.google.com/kh/v=67&x=' + matches[3] + '&y=' + matches[2] + '&z=' + matches[1] + '&s=Galileo'; // Replacement of the Reg Expression
+					nWorld[i].firstChild.setAttribute("src", rP);
+				}
+			}
+		}
+		
+		var rUS = /BBOX=(-?\d+\.\d+),(-?\d+\.\d+),/i; // Reg Expression for US server
+		var nUS = document.getElementById("DynamicWMS_9");
+		if (nUS != null) {
+			// We need to inject a script to retrieve data from Waze
+			var script = document.createElement('script');
+			script.innerHTML = 'var st = document.getElementById("googze_script"); st.innerHTML = "Googze state: <div id=\'googze_resolution\'>" + window.g_map.getResolution() + "</div><div id=\'googze_zoom\'>" + window.g_map.getZoom() + "</div><div id=\'googze_bounds_left\'>" + window.g_map.maxExtent.left + "</div><div id=\'googze_bounds_top\'>" + window.g_map.maxExtent.top + "</div>";';
+			document.getElementsByTagName('head')[0].appendChild(script);
+
+			nUS = nUS.childNodes; // Dom nodes for US server
+			// Replace US server
+			var res = document.getElementById('googze_resolution').innerHTML;
+			var bounds_left = document.getElementById('googze_bounds_left').innerHTML;
+			var bounds_top = document.getElementById('googze_bounds_top').innerHTML;
+ 			var z = document.getElementById('googze_zoom').innerHTML;
+			
+			for (var i in nUS) {
+				var src = nUS[i].firstChild.src;
+				var matches = src.match(rUS);
+				if (matches != null) {
+    					var x = Math.round ((bounds_left - matches[1]) / (res * 256));
+    					var y = Math.round ((matches[2] - bounds_top) / (res * 256));
+					
+					var rP = 'http://khm0.google.com/kh/v=67&x=' + x + '&y=' + y + '&z=' + z + '&s=Galileo'; // Replacement of the Reg Expression
+					nUS[i].firstChild.setAttribute("src", rP);
+				}
 			}
 		}
 	} catch(err) { console.log(err); };
 };
+
+var scriptDiv = document.createElement('div');
+scriptDiv.id = 'googze_script';
+document.body.appendChild(scriptDiv);
+
 wazeToGoogle();
