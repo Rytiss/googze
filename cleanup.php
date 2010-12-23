198739a331f9b67fca60657cd0c25c164a5bf4fe
rytis.s@gmail.com <rytis.s@gmail.com@b9383bd5-25d9-8f43-6282-5640fff02bcd>
2010-12-23 17:41:43 +0000

    Cache cleanup script to make sure we do not run out of disk space on server. Runs as a cron job

---
+++ cleanup.php
@@ -0,0 +1,31 @@
+<?php
+
+chdir(dirname(__FILE__));
+
+$files = scandir('cache');
+foreach ($files as $file) {
+	if (substr($file, 0, 1) == '.') { continue; }
+	if (disk_free_space(dirname(__FILE__)) < 100 * 1024 * 1024) {
+		rrmdir('cache/'.$file);
+	} else {
+		break;
+	}
+}
+
+
+function rrmdir($dir) {
+	if (is_dir($dir)) {
+		$objects = scandir($dir);
+		foreach ($objects as $object) {
+			if ($object != "." && $object != "..") {
+				if (filetype($dir."/".$object) == "dir") {
+					rrmdir($dir."/".$object);
+				} else {
+					unlink($dir."/".$object);
+				}
+			}
+		}
+		reset($objects);
+		rmdir($dir);
+	}
+}
